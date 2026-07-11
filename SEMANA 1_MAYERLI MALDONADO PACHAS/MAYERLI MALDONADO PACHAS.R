9028287460fcbeab47160b16f3f9e57af12c93d2
mayerlimaldonado769-max <mayerlimaldonado769@gmail.com>
2026-07-11 15:49:16 -0500

    Add files via upload

---
+++ SEMANA 1_MAYERLI MALDONADO PACHAS/MAYERLI MALDONADO PACHAS.R
@@ -0,0 +1,6 @@
+# Proyecto-semana 1
+
+#RStudio es un entorno de desarrollo integrado (IDE) de código abierto 
+#diseñado específicamente para el lenguaje de programación R, utilizado en computación 
+#estadística y gráficos. Sirve para escribir, editar, ejecutar y depurar código de manera eficiente
+#ilitando enormemente el análisis de datos, la manipulación de bases de datos y la creación de gráficos interactivos.
